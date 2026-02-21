6cdb7b033f41de250d19de153cf8921e824d64ae
rajat-sharma8181 <rajats12120@gmail.com>
2026-02-21 22:35:48 +0530

    commented subid

--- dev/provider.tf
+++ dev/provider.tf
@@ -5,7 +5,7 @@ terraform {
     container_name       = "backendcontainer"                               
     key                  = "dev.terraform.tfstate"                
   }
-  
+
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
@@ -16,5 +16,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "983c99f2-6e09-45df-bbf3-b94a4d7e5a84"
+  # subscription_id = "983c99f2-6e09-45df-bbf3-b94a4d7e5a84"
 }
