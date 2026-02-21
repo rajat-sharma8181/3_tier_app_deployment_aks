45a4aa94e574150ef2ae3b85f66a159c1df59d17
rajat-sharma8181 <rajats12120@gmail.com>
2026-02-21 22:50:05 +0530

    object id corrected

--- modules/aks_acr_role/main.tf
+++ modules/aks_acr_role/main.tf
@@ -2,5 +2,5 @@ resource "azurerm_role_assignment" "acr_pull" {
     for_each = var.aks_acr_pull
   scope                = data.azurerm_container_registry.acr_data[0].id
   role_definition_name = each.value.role_definition_name
-  principal_id         = data.azurerm_kubernetes_cluster.aks_data[0].object_id
+  principal_id = data.azurerm_kubernetes_cluster.aks_data.kubelet_identity[0].object_id
 }
