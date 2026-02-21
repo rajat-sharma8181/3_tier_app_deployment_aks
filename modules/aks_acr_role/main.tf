688218a74a94d0208eeb147417d2f1a94da0ae4a
rajat-sharma8181 <rajats12120@gmail.com>
2026-02-21 22:56:28 +0530

    modified principal id

--- modules/aks_acr_role/main.tf
+++ modules/aks_acr_role/main.tf
@@ -2,5 +2,5 @@ resource "azurerm_role_assignment" "acr_pull" {
     for_each = var.aks_acr_pull
   scope                = data.azurerm_container_registry.acr_data[each.key].id
   role_definition_name = each.value.role_definition_name
-  principal_id = data.azurerm_kubernetes_cluster.aks_data.kubelet_identity[each.key].object_id
+  principal_id = data.azurerm_kubernetes_cluster.aks_data[each.key].kubelet_identity[0].object_id
 }
