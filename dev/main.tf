9e74cead47f54d2311895676cb04c006ca1a9533
rajat-sharma8181 <rajats12120@gmail.com>
2026-02-23 00:05:50 +0530

    dependency added

--- dev/main.tf
+++ dev/main.tf
@@ -9,11 +9,13 @@ module "rg_mod" {
 # }
 
 module "acr_mod" {
+  depends_on = [ module.rg_mod ]
   source = "../modules/acr"
   acr = var.acr_dev
 }
 
 module "aks_mod" {
+  depends_on = [ module.rg_mod, module.acr_mod ]
   source = "../modules/aks_cluster"
   aks_cluster = var.aks_cluster_dev
 }
@@ -24,7 +26,7 @@ module "aks_mod" {
 # }
 
 module "aks_acr_pull_mod" {
-  depends_on = [ module.acr_mod, module.aks_mod ]
+  depends_on = [ module.rg_mod, module.acr_mod, module.aks_mod ]
   source = "../modules/aks_acr_role"
   aks_acr_pull = var.aks_acr_pull_dev
 }
